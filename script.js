a1953a6ffd1d93d83121b8cd39328c04ae21086c
Deepak <chauhandeephp@gmail.com>
2021-05-18 09:59:05 +0530

    get requests via callback hell

--- script.js
+++ script.js
@@ -5,19 +5,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (countryName) {
-
-const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.eu/rest/v2/name/${countryName}`);
-request.send();
-
-request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-
-    // const data = JSON.parse(this.responseText)[1];
-    
+const renderCountry = function(data, className= '') {
     const html = `
-        <article class="country">
+        <article class="country ${className}">
             <img class="country__img" src="${data.flag}" />
                 <div class="country__data">
                     <h3 class="country__name">${data.name}</h3>
@@ -31,8 +21,40 @@ request.addEventListener('load', function () {
 
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
+}
+
+const getCountryData = function (countryName) {
+
+    // Ajax Call 1
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.eu/rest/v2/name/${countryName}`);
+    request.send();
+
+    // Request for load the data
+    request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    // const data = JSON.parse(this.responseText)[1];
+    renderCountry(data);
+
+    // Get Neighbour Country
+    const neighbour = data.borders[1];
+    console.log(neighbour);
+
+    if(!neighbour) return;
+
+     // Ajax Call 2
+     const request2 = new XMLHttpRequest();
+     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+     request2.send();
+
+    request2.addEventListener('load', function() {
+        const data2 = JSON.parse(this.responseText);
+        console.log(data2);
+
+        renderCountry(data2, 'neighbour')
+    })
+
 });
 };
 
-getCountryData('bhutan');
-getCountryData('nepal');
+getCountryData('usa');
